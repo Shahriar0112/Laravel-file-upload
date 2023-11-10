853168622ff31cb6ca20bf3e72a903936e7f1a77
Shahriar0112 <118440851+Shahriar0112@users.noreply.github.com>
2023-11-10 21:36:10 +0600

    Laravel-Project

---
+++ app/Http/Controllers/OstadController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class OstadController extends Controller
+{
+    //
+
+    function sayHi(){
+        return "Welcome To First Laravel Project";
+    }
+}
